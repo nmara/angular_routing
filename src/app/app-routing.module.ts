a95b945b11ff5e9549f18750edf5ba63fd5dbfc5
nmara <aga.czekala89@gmail.com>
2018-06-28 16:10:29 +0200

    end of section + useHash'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AuthGuard } from './auth-guard.service';
 import { CanDeavtivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -23,7 +24,7 @@ const appRoutes: Routes = [
   canActivateChild: [AuthGuard],
   component: ServersComponent,
   children: [
-    { path: ':id', component: ServerComponent },
+    { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
     { path: ':id/edit', component: EditServerComponent, canDeactivate:[CanDeavtivateGuard] },
   ] },
   // { path: 'not-found', component: PageNotFoundComponent },
@@ -33,6 +34,7 @@ const appRoutes: Routes = [
 
 @NgModule ({
   imports: [
+    // RouterModule.forRoot(appRoutes, {useHash: true}) // tylko do starych przeglądarek
     RouterModule.forRoot(appRoutes)
   ],
   exports: [RouterModule]
